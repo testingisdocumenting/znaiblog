d41da22520a0f5af5e9f57b9c04849b7d2cc22f5
Mykola Golubyev <golubev.nikolay@gmail.com>
2019-11-12 13:11:52 -0500

    use znai documentationNavigation to navigate from landing (#12)

--- znaiblog-reactjs/src/landing/ListOfBlogEntries.tsx
+++ znaiblog-reactjs/src/landing/ListOfBlogEntries.tsx
@@ -4,6 +4,8 @@ import {TocItem} from '../model/TocItem';
 
 import {renderDate} from '../utils/dateUtils';
 
+import {documentationNavigation} from '../structure/documentationNavigation';
+
 import './ListOfBlogEntries.css';
 
 interface Props {
@@ -21,11 +23,20 @@ export function ListOfBlogEntries({tocItems}: Props) {
 function SingleEntry(entry: TocItem) {
     return (
         <div className="blog-list-entry">
-            <a className="blog-list-entry-link-wrapper" href={`${entry.dirName}/${entry.fileName}`}>
+            <a className="blog-list-entry-link-wrapper"
+               href={documentationNavigation.buildUrl(entry)}
+               onClick={onBlogEntryClick(entry)}>
                 <div className="blog-list-entry-title">{entry.pageTitle}</div>
                 <div className="blog-list-entry-date">{renderDate(entry.pageMeta.date![0])}</div>
                 <div className="blog-list-entry-summary">{entry.pageMeta.summary}</div>
             </a>
         </div>
     )
+}
+
+function onBlogEntryClick(entry: TocItem) {
+    return (e: React.MouseEvent) => {
+        e.preventDefault();
+        documentationNavigation.navigateToPage(entry);
+    }
 }
