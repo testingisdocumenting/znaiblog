4db0c5635721680d8703217d50fcd8dcb3282206
Mykola Golubyev <golubev.nikolay@gmail.com>
2019-11-15 20:45:02 -0500

    add dynamic theme switching (#15)

---
+++ znaiblog-reactjs/src/layout/ThemeSwitch.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {themeRegistry} from '../znaiapi/themeRegistry';
+
+import './ThemeSwitch.css';
+
+export class ThemeSwitch extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="znaiblog-theme-switch">
+                <div className="znaiblog-theme-switch-action" onClick={this.switchTheme}>
+                    switch to {this.switchToLabel()}
+                </div>
+            </div>
+        );
+    }
+
+    switchTheme = () => {
+        console.log('switchTheme');
+
+        console.log(themeRegistry.currentTheme);
+
+        const newTheme = themeRegistry.currentTheme.name === 'default' ? 'znai-dark' : 'default';
+        console.log('new theme', newTheme);
+
+        themeRegistry.selectTheme(newTheme);
+        this.forceUpdate();
+    };
+
+    switchToLabel = () => {
+        return themeRegistry.currentTheme.name === 'default' ? 'dark' : 'light';
+    }
+}
